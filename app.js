1b662508ebac6a69a3d00f93579e0a1758eae3e8
ZamanHuseyn <zamanhuseyn2009@gmail.com>
2025-12-18 16:56:26 +0400

    homework

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-function isValidNumber(num){
-let s = String(num);
-return s.length === 12 && s.startsWith("+7")
+let a = 100;
+while (a < 1000) {
+    console.log(a);
+    a += 10;
 }
-console.log(isValidNumber("+71213112223"))
 
-function numCounter(count){
-   let s = String(count);
-   return s.length
-}
-console.log(numCounter("SALAM"))
 
-function capitalizeWords(text) {
-  return text.replace(/\b\w/g, char => char.toUpperCase());
+
+
+let b = 11;
+let sum = 0;
+
+while (b < 100) {
+    sum += i;
+    b += 2;
 }
-console.log(capitalizeWords("Hey my name is Mirhuseyn"))
+
+console.log(sum);
